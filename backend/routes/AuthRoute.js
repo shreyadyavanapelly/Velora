ec246527426d111b64814038928d3fe9318dad40
shreyadyavanapelly <shreyaraodyavanapelly@gmail.com>
2026-07-27 22:07:08 +0530

    changed capital to small

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -1,4 +1,4 @@
-const { Signup, Login } = require('../Controllers/AuthController')
+const { Signup, Login } = require('../controllers/AuthController')
 const router = require('express').Router()
 
 router.post('/signup', Signup)
